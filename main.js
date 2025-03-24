2e6fd45f82709348b505269322ebe5d5f67ff92d
Chandler Goudreau - Patterson <105453078+git-it-chan@users.noreply.github.com>
2025-03-24 09:53:09 -0400

    Add more pages
    Create and code new html pages

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("navbar").innerHTML = data;
+    });
+
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
